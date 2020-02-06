80d3861b044e8680b7d4083d40c9c5ecb7aa805e
Aimee <10893245@my.uvu.edu>
2020-02-05 19:10:28 -0700

    Trigger Enter, Trigger Exit Script

---
+++ New Unity Project (1)/Assets/Triggerevent.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine.Events;
+using UnityEngine;
+[RequireComponent(typeof(Collider))]
+public class Triggerevent : MonoBehaviour
+{
+    private void Awake()
+       {
+           GetComponent<Collider>().isTrigger = true;
+       }
+   
+       public UnityEvent triggerEnterEvent, triggerExitEvent;
+       private void OnTriggerEnter(Collider other)
+       {
+           triggerEnterEvent.Invoke();
+           
+       }
+   
+       private void OnTriggerExit(Collider other)
+       {
+           triggerExitEvent.Invoke();
+       }
+   }
+
