d3e5085bcde75f624bb33c38f9e4048b2fc1855f
Aimee <10893245@my.uvu.edu>
2020-02-07 16:39:09 -0700

    Mono Behaviour Script

---
+++ New Unity Project (1)/Assets/MonoBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Events;
+
+public class MonoBehavior : MonoBehaviour
+{
+    public class MonoEvents : MonoBehaviour
+    {
+        public UnityEvent startEvent;
+        void Start()
+        {
+            startEvent.Invoke();
+        }
+    }
+}
