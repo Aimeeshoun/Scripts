2fce64a1a4c1a71e419f7ce61f2b8a39dcd1401c
Aimee <10893245@my.uvu.edu>
2020-02-08 19:38:47 -0700

    Vector3Data and Applied Force

---
+++ New Unity Project (1)/Assets/force.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+[RequireComponent(typeof(Collider))]
+
+public class force : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public Vector3Data theForceDirection;
+
+    public float theForce = 6f;
+
+
+
+    void Start()
+
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(theForceDirection.value * theForce);
+
+    }
+
+
+
+}
