c042a395e5cd770d762a42e985b58f2bf1b381ea
Aimee <10893245@my.uvu.edu>
2020-02-09 11:32:49 -0700

    If statement experiment script

---
+++ New Unity Project (1)/Assets/Ifstatment.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class Ifstatment : MonoBehaviour
+{
+    
+    public int a1 = 3;
+    public int b2 = 4;
+    public int c3 = 9;
+   
+    public bool lightsOn = false;
+
+    void Start()
+    {
+        if (a1+b2 < c3)
+        {
+            print(true);
+        }
+      
+         else if (!lightsOn)
+        {
+            print(true);
+        }
+        else
+        {
+            print(false);
+        }
+        
+    }
+    
+    
+}
+
+
+
