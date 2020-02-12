752dc16157a712c20ef273a258c169f31e22b817
Aimee <10893245@my.uvu.edu>
2020-02-11 17:40:25 -0700

    Game Art Data Script

---
+++ New Unity Project (1)/Assets/GameArtData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class GameArtData : ScriptableObject
+{
+    
+
+    public Sprite sprite;
+
+    public Color spriteColor;
+
+    public GameObject prefab;
+
+}
+
