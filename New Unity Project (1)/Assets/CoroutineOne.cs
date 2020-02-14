7b0ff20bbd6c89d6311918f6eec6fec618ad190e
Aimee <10893245@my.uvu.edu>
2020-02-14 06:20:17 -0700

    Coroutine Script

---
+++ New Unity Project (1)/Assets/CoroutineOne.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+public class CoroutineOne : MonoBehaviour
+{
+   
+    public class SelfDestruct : MonoBehaviour
+
+    {
+
+        public float theDelay = 4f;
+
+        void Start()
+
+        {
+
+            Destroy(gameObject, theDelay);
+
+        }
+
+
+
+        public void DestroyOnImpact()
+
+        {
+
+            Destroy(gameObject);
+
+        }
+
+    }
+}
