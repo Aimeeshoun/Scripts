7989dcb033061573f07a80dd84d21c4de908570b
Aimee <10893245@my.uvu.edu>
2020-02-15 13:09:44 -0700

    Float Data Script and UI Controller

---
+++ New Unity Project (1)/Assets/FloatData.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+  
+  
+  public float theValue = 1f;
+        public float theMinValue = 0;
+        public float maxValue = 1f;
+    
+        public void UpdateValue(float amount)
+        {
+            theValue += amount;
+        }
+
+        public void UpdateValueRange (float amount)
+        {
+            if (theValue <= maxValue)
+            {
+                UpdateValue(amount);
+            }
+            else
+            {
+                theValue = maxValue;
+            }
+        
+            if (theValue >= theMinValue)
+            {
+                UpdateValue(amount);
+            }
+            else
+            {
+                theValue = theMinValue;
+            }
+        
+        }
+    }
---
+++ New Unity Project (1)/Assets/UIcontroller.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI; 
+[RequireComponent(typeof(Image))]
+public class UIcontroller : MonoBehaviour
+{
+
+       private Image img;
+       void Awake()
+       {
+           img = GetComponent<Image>();
+       }
+   
+       public void UpdateImage(FloatData data)
+       {
+           img.fillAmount = data.theValue;
+       }
+   }
+
