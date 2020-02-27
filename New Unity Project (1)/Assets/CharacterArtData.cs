6ba129a5c877426c16b77d1e1ced9235f5e3c7b7
Aimee <10893245@my.uvu.edu>
2020-02-27 12:43:43 -0700

    Character art data

---
+++ New Unity Project (1)/Assets/CharacterArtData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class CharacterArtData : ScriptableObject
+
+    {
+    
+
+        public Sprite spriteOutfit;
+
+        public Sprite characterSprite;
+
+        public Color spriteOutfitColor;
+
+        public GameObject prefabOutfit;
+
+    }
+
