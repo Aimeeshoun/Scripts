301d95de1e262edbea873e37e72f31fffc1b4c1d
Aimee <10893245@my.uvu.edu>
2020-02-28 20:18:55 -0700

    CoinPickUp Script

---
+++ New Unity Project (1)/Assets/coinpickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinpickup : MonoBehaviour
+
+   
+    {
+        public bool enter = true;
+        private float theCoinAmount;
+        private float amount;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player1"))
+            {
+                amount = theCoinAmount;
+                theCoinAmount += 1;
+                Destroy(other.gameObject);
+                Debug.Log("You have " + theCoinAmount + " coins!");
+            }
+        }
+    }
+
