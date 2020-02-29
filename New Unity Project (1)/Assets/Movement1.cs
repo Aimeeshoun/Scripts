26e42cde192a681deb98a719af65536e227ebc0a
Aimee <10893245@my.uvu.edu>
2020-02-29 10:43:09 -0700

    Movement Script

---
+++ New Unity Project (1)/Assets/Movement1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement1 : MonoBehaviour
+{
+      public float moveSpeed = 8f;
+    public float jumpSpeed = 32f;
+    public float gravity = 9.81f;
+    private int jumpCountMax = 9;
+    private Vector3 playerPosition;
+    private CharacterController myController;
+    public int jumpCount;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        myController = GetComponent<CharacterController>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition.y -= gravity;
+        playerPosition.x = moveSpeed *Input.GetAxis("Horizontal");
+
+        if (myController.isGrounded)
+        {
+            playerPosition.y = 0;
+            jumpCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        {
+            playerPosition.y = jumpSpeed;
+            jumpCount++;
+        }
+
+        myController.Move(motion: playerPosition*Time.deltaTime);
+    }
+}
+
