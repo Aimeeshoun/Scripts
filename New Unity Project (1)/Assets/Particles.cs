fbcfc29f0658646e67a7130f783ec4743202a66f
Aimee <10893245@my.uvu.edu>
2020-03-02 22:20:35 -0700

    Particle Script

---
+++ New Unity Project (1)/Assets/Particles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particles : MonoBehaviour
+{
+   
+    
+   
+        public ParticleSystem theParticles;
+        public float value = 2f;
+
+        public void UpdateValue(float amount)
+        {
+            value += amount;
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+            }
+
+            theParticles.Emit(700);
+        }
+
+      
+   
+    }
