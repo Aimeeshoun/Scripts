215161d293fc0d0ac1f17ee0ee53253ee533296e
Aimee <10893245@my.uvu.edu>
2020-03-04 15:36:32 -0700

    Trigger Event Script

---
+++ New Unity Project (1)/Assets/TriggerEvent1.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class TriggerEvent1 : MonoBehaviour
+
+    {
+        private void Awake()
+        {
+            GetComponent<Collider>().isTrigger = true;
+        }
+
+        public UnityEvent triggerEnterEvent, triggerExitEvent;
+
+        public void OnTriggerEnter(Collider other)
+        {
+            triggerEnterEvent.Invoke();
+        }
+
+        public void OnTriggerExit(Collider other)
+        {
+            triggerExitEvent.Invoke();
+        }
+    }
