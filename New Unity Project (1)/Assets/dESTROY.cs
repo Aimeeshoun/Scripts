277569ca96c94f832c4478ea8c997d515164edab
Aimee <10893245@my.uvu.edu>
2020-03-07 20:26:04 -0600

    Emit Particles

--- New Unity Project (1)/Assets/dESTROY.cs
+++ New Unity Project (1)/Assets/dESTROY.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 
 public class dESTROY : MonoBehaviour
 {
+    public ParticleSystem particles;
     private void OnTriggerEnter(Collider other)
     {
         Destroy(this.gameObject);
+        particles.Emit(100);
     }
 
     
