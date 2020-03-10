71d9e47a456d2cf1cd509c1c752fddb332a1e737
Aimee <10893245@my.uvu.edu>
2020-03-09 20:05:10 -0600

    instantiate

---
+++ New Unity Project (1)/Assets/instaciate.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class instaciate : MonoBehaviour
+{
+
+   
+       
+       private int bulletCount = 8;
+       [SerializeField] private GameObject laserPrefab;
+       private float _speed = 10f;
+      
+       void Update()
+       {
+           if (Input.GetKeyDown(KeyCode.RightShift))
+           {
+               Instantiate(laserPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z),
+                   Quaternion.identity);
+                      
+           }
+       }
+   }
