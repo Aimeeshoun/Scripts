a889ec8c58bc5bd19bff507939db15aad0b03f3f
Aimee <10893245@my.uvu.edu>
2020-03-11 17:42:11 -0600

    Cinemachine

---
+++ New Unity Project (1)/Assets/TriggerEvent2.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+[RequireComponent(typeof(Collider))]
+public class TriggerEvent2 : MonoBehaviour
+
+
+
+{
+        private void Awake()
+        {
+            GetComponent<Collider>().isTrigger = true;
+        }
+
+        public UnityEvent triggerEvent;
+        private void OnTriggerEnter(Collider other)
+        {
+            triggerEvent.Invoke();
+        }
+    }
