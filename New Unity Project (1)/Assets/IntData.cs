fe4c13c3e33213994c3ddd61baf330252489cab0
Aimee <10893245@my.uvu.edu>
2020-03-14 17:41:32 -0600

    Intdata script

---
+++ New Unity Project (1)/Assets/IntData.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu]
+public class IntData : MonoBehaviour
+
+    {
+
+        public int value = 2;
+
+
+
+        public void UpdateValue(int number)
+
+        {
+
+            value += number;
+
+        }
+
+    }
